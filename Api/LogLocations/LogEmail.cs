15a1ec53ac590dd8240414244c0d2eca111d0fc3
Lauras Dilys <lauras.dilys@gmail.com>
2021-09-27 01:56:40 +0300

    "LogToEmail" simulation implemented

--- Api/LogLocations/LogEmail.cs
+++ Api/LogLocations/LogEmail.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Dto;
 using Business.Interfaces;
+using System;
+using System.Text.Json;
 
 namespace Api.LogLocations
 {
@@ -7,7 +9,20 @@ namespace Api.LogLocations
     {
         public void Create(LogRequest request)
         {
-            // Tipo email this request
+            // Simuliuojamas Email išsiuntimas
+
+            Console.WriteLine($"--Email sent on {DateTime.Now}--\n");
+            Console.WriteLine("Recently added logs:\n");
+
+            foreach (var item in request.Events)
+            {
+                Console.WriteLine($"Timestamp: {item.Timestamp}");
+                Console.WriteLine($"Level: {item.Level}");
+                Console.WriteLine($"MessageTemplate: {item.MessageTemplate}");
+                Console.WriteLine($"RenderedMessage: {item.RenderedMessage}");
+                Console.WriteLine($"Properties: {JsonSerializer.Serialize(item.Properties)}");
+                Console.WriteLine();
+            }
         }
     }
 }
